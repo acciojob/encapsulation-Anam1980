8c9242f9c8cebb4d198eac945ba4a4404f65c1cf
Your Name <you@example.com>
2023-08-12 13:48:18 +0530

    Add ENCP File

---
+++ src/main/java/pack/RWOnly.java
@@ -0,0 +1,15 @@
+public class RWOnly {
+    private String name;//private variable name
+
+    //setter to set the name
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //getter to get the name
+
+    public String getName() {
+        return name;
+    }
+}
