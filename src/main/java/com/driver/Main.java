b5e292dd1b6b7f0f8a75176284be42e71fb0760f
Your Name <you@example.com>
2023-08-12 13:52:24 +0530

    Add newFile

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,11 +1,11 @@
 public class Main {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         RWOnly rwOnly = new RWOnly();//call the class by object
 
         //set the name
         rwOnly.setName("Tony Stark");
 
         //get the name
-        System.out.println("Name: "+rwOnly.getName());
+        System.out.println("Name: " + rwOnly.getName());
     }
 }
